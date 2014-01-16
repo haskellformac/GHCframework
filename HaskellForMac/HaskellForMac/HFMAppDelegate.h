dfecde57c10ee2009304cef53ab7f173b7754c02
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2014-01-16 23:12:08 +1100

    Create Xcode project

---
+++ HaskellForMac/HaskellForMac/HFMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HFMAppDelegate.h
+//  HaskellForMac
+//
+//  Created by Manuel M T Chakravarty on 16/01/2014.
+//  Copyright (c) 2014 Manuel M T Chakravarty. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HFMAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ HaskellForMac/HaskellForMacTests/HaskellForMacTests.m
@@ -0,0 +1,34 @@
+//
+//  HaskellForMacTests.m
+//  HaskellForMacTests
+//
+//  Created by Manuel M T Chakravarty on 16/01/2014.
+//  Copyright (c) 2014 Manuel M T Chakravarty. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HaskellForMacTests : XCTestCase
+
+@end
+
+@implementation HaskellForMacTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
