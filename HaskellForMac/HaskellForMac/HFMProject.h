6d9182f07512bf8bf10a271503a3c73e4ca42488
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2014-01-17 16:33:23 +1100

    Initial window layout

---
+++ HaskellForMac/HaskellForMac/HFMProject.h
@@ -0,0 +1,15 @@
+//
+//  HFMProject.h
+//  HaskellForMac
+//
+//  Created by Manuel M T Chakravarty on 17/01/2014.
+//  Copyright (c) 2014 Manuel M T Chakravarty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HFMProject : NSObject<NSCopying>
+
+@property (readonly, nonatomic) NSString *name;
+
+@end
---
+++ HaskellForMac/HaskellForMac/HFMProject.m
@@ -0,0 +1,13 @@
+//
+//  HFMProject.m
+//  HaskellForMac
+//
+//  Created by Manuel M T Chakravarty on 17/01/2014.
+//  Copyright (c) 2014 Manuel M T Chakravarty. All rights reserved.
+//
+
+#import "HFMProject.h"
+
+@implementation HFMProject
+
+@end
