d2bf3ae59e866a857ce434f8b791462dcbd77d6c
Manuel M T Chakravarty <chak@justtesting.org>
2014-08-06 15:51:17 +1000

    Line numbers in the editor view
    Fixes #81

--- HaskellForMac/HaskellForMac/TextGutterView.swift
+++ HaskellForMac/HaskellForMac/TextGutterView.swift
@@ -15,11 +15,12 @@ class TextGutterView: NSRulerView {
 
   // Text attributes for line numbers
   //
-  private let textAttributes = [NSFontAttributeName: NSFont.systemFontOfSize(NSFont.smallSystemFontSize())]
+  private let textAttributes = [NSFontAttributeName:            NSFont.systemFontOfSize(NSFont.smallSystemFontSize()),
+                                NSForegroundColorAttributeName: NSColor(deviceWhite: 0.5, alpha: 1)]
 
   // Gap between the line numbers and the bounding box of the ruler.
   //
-  private let margin: CGFloat = 4
+  private let margin: CGFloat = 3
 
 
   // MARK: -
@@ -31,6 +32,8 @@ class TextGutterView: NSRulerView {
     }
 
     super.init(scrollView: scrollView, orientation: orientation)
+
+    self.clientView = scrollView.documentView as NSView
   }
 
   required init(coder: NSCoder) {
@@ -47,7 +50,39 @@ class TextGutterView: NSRulerView {
   }
 
   override func drawHashMarksAndLabelsInRect(rect: NSRect) {
-    drawLineNumber(42, middleline: rect.origin.y)
+    let textView      = self.clientView as NSTextView
+    let layoutManager = textView.layoutManager
+    let textContainer = textView.textContainer
+    let string        = textView.textStorage.string
+    let visibleRect   = self.scrollView.documentVisibleRect
+
+      // All visible glyphs and all visible characters
+    let glyphRange    = layoutManager.glyphRangeForBoundingRectWithoutAdditionalLayout(visibleRect,
+                                                                                       inTextContainer: textContainer)
+    let charRange     = layoutManager.characterRangeForGlyphRange(glyphRange, actualGlyphRange: nil)
+
+      // Line number of first visible line
+    let firstLineNumber = string.lineNumberAtLocation(charRange.location)
+
+      // Iterate through the line numbers of all visible lines
+      //
+      // NB: The line number of the first visible line may not be visble (if the line wraps), but clipping handles that
+      //     for us.
+    var lineNumber = firstLineNumber
+    var charIndex  = charRange.location
+    while charIndex < NSMaxRange(charRange) {
+
+      let lineRange = (string as NSString).lineRangeForRange(NSRange(location: charIndex, length: 0))
+
+        // Draw the number for the current line
+      let offset = layoutManager.lineFragmentRectForGlyphAtIndex(layoutManager.glyphIndexForCharacterAtIndex(lineRange.location),
+                                                                 effectiveRange: nil)
+      drawLineNumber(lineNumber, middleline: offset.origin.y - visibleRect.origin.y + offset.size.height / 2)
+
+        // Advance to next line
+      lineNumber++
+      charIndex = NSMaxRange(lineRange)
+    }
   }
 
   // Draws the given number in the gutter, such that the glyphs' baseline is as specified. The baseline is given
@@ -59,5 +94,14 @@ class TextGutterView: NSRulerView {
     numberString.drawAtPoint(NSPoint(x: ruleThickness - margin - size.width,
                                      y: middleline - size.height / 2),
                              withAttributes: textAttributes)
+  }
+}
+
+extension NSString {
+  func lineNumberAtLocation(loc: Int) -> Int {
+    switch self.lineRangeForRange(NSRange(location: loc, length: 0)).location {
+    case 0: return 1
+    case let start: return lineNumberAtLocation(start - 1) + 1
+    }
   }
 }
--- HaskellForMac/HaskellForMacTests/Experiments.playground/section-1.swift
+++ HaskellForMac/HaskellForMacTests/Experiments.playground/section-1.swift
@@ -2,4 +2,43 @@
 
 import Cocoa
 
-NSLog("Hallo")
+let myText = "This is a long piece of text, which is spread\n\n\nover multiple lines,\nand we need to figure out how many lines\nthere are."
+
+myText.rangeOfString("which")
+let range: Range<String.Index> = advance(myText.startIndex, 47)...advance(myText.startIndex, 49)
+
+myText
+myText.lineRangeForRange(range)
+myText.lineRangeForRange(myText.startIndex...myText.startIndex)
+myText.startIndex
+
+extension String {
+  func lineNumberAtLocation(loc: String.Index) -> UInt {
+    switch self.lineRangeForRange(loc...loc).startIndex {
+    case self.startIndex: return 1
+    case let start: return lineNumberAtLocation(advance(start, -1)) + 1
+    }
+  }
+}
+
+myText.lineNumberAtLocation(advance(myText.startIndex, 66))
+myText[myText.startIndex...advance(myText.startIndex, 66)]
+
+let myNSText = myText as NSString
+let r = myNSText.rangeOfString("which")
+
+myNSText.lineRangeForRange(NSRange(location: 0, length: 0))
+myNSText.characterAtIndex(0)
+
+("a\n\n" as NSString).lineRangeForRange(NSRange(location: 2, length: 1))
+
+extension NSString {
+  func lineNumberAtLocation(loc: Int) -> Int {
+    switch self.lineRangeForRange(NSRange(location: loc, length: 0)).location {
+    case 0: return 1
+    case let start: return lineNumberAtLocation(start - 1) + 1
+    }
+  }
+}
+
+myNSText.lineNumberAtLocation(67)
