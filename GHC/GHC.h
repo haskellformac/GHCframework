365149726ab3057b9d81475d4b9897e865f99e8f
Manuel M T Chakravarty <chak@justtesting.org>
2014-10-23 15:02:19 +1100

    Create GHC.framework project
    Contributes to #204

---
+++ GHC/GHC.h
@@ -0,0 +1,19 @@
+//
+//  GHC.h
+//  GHC
+//
+//  Created by Manuel M T Chakravarty on 23/10/2014.
+//  Copyright (c) 2014 Manuel M T Chakravarty. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GHC.
+FOUNDATION_EXPORT double GHCVersionNumber;
+
+//! Project version string for GHC.
+FOUNDATION_EXPORT const unsigned char GHCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GHC/PublicHeader.h>
+
+
