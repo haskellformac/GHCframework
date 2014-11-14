896b69c4bb0cd51a4694d497bc00002cacb678e6
Manuel M T Chakravarty <chak@justtesting.org>
2014-11-14 11:46:36 +1100

    Standardise paths going into package configs
    Fixes #216
    
    (Though, we still need to sign manually with the 3rd party Mac
    Developer Application key — currently,
    8C966227D12305A8C94A6F010F3F9202476DC553.)

---
+++ RelocateGHC/StandardizePath/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  StandardizePath
+//
+//  Created by Manuel M T Chakravarty on 14/11/2014.
+//  Copyright (c) 2014 Manuel M T Chakravarty. All rights reserved.
+//
+//  Uses the 'NSString' operation 'stringByResolvingSymlinksInPath' on its first argument to produce a canonical path.
+
+import Foundation
+
+println(Process.arguments[1].stringByResolvingSymlinksInPath)
+
