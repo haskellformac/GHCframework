1d05f8fcc4cc5a5db0c37cf706a18c464f29c00c
Manuel M T Chakravarty <chak@justtesting.org>
2015-01-26 15:09:54 +1100

    Documentation window
    * Documentation window
    * Menu entry pointing to haskell.org
    Contributes to #237

---
+++ HaskellForMac/HaskellForMac/DocumentationController.swift
@@ -0,0 +1,53 @@
+//
+//  DocumentationController.swift
+//  HaskellForMac
+//
+//  Created by Manuel M T Chakravarty on 26/01/2015.
+//  Copyright (c) 2015 Manuel M T Chakravarty. All rights reserved.
+//
+
+import Cocoa
+import WebKit
+
+
+let documentationPath = "Contents/Frameworks/GHC.framework/Versions/Current/usr/share/doc/ghc/html/libraries/index.html"
+let documentationRoot = NSBundle.mainBundle().bundlePath.stringByAppendingPathComponent(documentationPath)
+let haskellPortal     = NSURL(string: "https://haskell.org/")!
+
+class DocumentationController: NSWindowController {
+
+  @IBOutlet private weak var webView: WebView!
+
+  // The nib containing the associated window.
+  //
+  override var windowNibName : String! {
+    return "Documentation"
+  }
+
+  // Reopen the preferences window when the app's persistent state is restored.
+  //
+  class func restoreWindowWithIdentifier(identifier: String,
+    state: NSCoder,
+    completionHandler: (NSWindow, NSError!) -> Void) -> Bool
+  {
+    completionHandler(((NSApp as NSApplication).delegate as AppDelegate).preferencesController.window!, nil)
+    return true
+  }
+
+  override func windowDidLoad() {
+    super.windowDidLoad()
+
+    window?.identifier       = "Documentation"
+    window?.restorationClass = PreferencesController.self
+    window?.title            = "Haskell Library Documentation"
+
+    webView.mainFrame.loadRequest(NSURLRequest(URL: NSURL(string: documentationRoot)!))
+  }
+}
+
+extension DocumentationController {
+
+  @IBAction func gotoHaskellPortal(sender: NSMenuItem) {
+    NSWorkspace.sharedWorkspace().openURL(haskellPortal)
+  }
+}
