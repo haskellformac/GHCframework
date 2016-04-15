fde2ffe366ba443d11c72c0b16dfc9846f8c06e7
Manuel M T Chakravarty <chak@justtesting.org>
2016-04-15 17:28:06 +1000

    Add cpphs
    * As with ghc-pkg, we have two versions: a signed one for use by HfM
    and an unsigned one for use by the CLI

---
+++ HaskellCLI/cpphs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+appContainer="$HOME/Library/Containers/com.haskellformac.Haskell.basic/Data/Library/Application Support"
+libGHC="/usr/local/lib/ghc-VERSION"
+exedir="$appContainer/lib/ghc/bin"
+exeprog="cpphs"
+topdir="$appContainer/lib/ghc"
+executablename="$exedir/$exeprog"
+if [ ! "$executablename" -ef "$libGHC/bin/$exeprog" ]; then
+$libGHC/SetupCLI $libGHC/bin || exit 1
+fi
+exec "$executablename" ${1+"$@"}
