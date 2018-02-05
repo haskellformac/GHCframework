76daf954e777b941bb4f29f360a76c4df3750d4d
Manuel M T Chakravarty <chak@justtesting.org>
2018-02-05 22:00:34 +1100

    Warning police

--- GHC/GHC.swift
+++ GHC/GHC.swift
@@ -20,7 +20,7 @@ final public class GHC: NSObject {
   public class var haskellVersion: (String, String, String) { get {
     let bundle        = Bundle(for: GHC.self),
         bundleVersion = (bundle.infoDictionary?["CFBundleShortVersionString"] as? String) ?? "unknown",
-        components    = bundleVersion.characters.split(omittingEmptySubsequences: false){ $0 == "-" }.map { String($0) }
+        components    = bundleVersion.split(omittingEmptySubsequences: false){ $0 == "-" }.map { String($0) }
     if components.count == 3 {
       return (components[0], components[1], components[2])
     } else { return ("", "", bundleVersion) }
